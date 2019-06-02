c8a88a99d4eee6a127bb05b4ca619652e2e114b3
Eliot Howes <ejhowes@gmail.com>
2019-06-02 16:58:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,7 +125,7 @@ end
 
 def big_shoe_rebounds
   biggest_shoes = {}
-  rebounds = ""
+  # rebounds = ""
   game_hash.each do |home_away, team_data|
     team_data.each do |team, attributes|
       if team == :players
@@ -147,5 +147,5 @@ def big_shoe_rebounds
       end
     end
   end
-  return rebounds
+  # return rebounds
 end
