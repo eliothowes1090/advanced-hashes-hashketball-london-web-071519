c27bf3e45705d043b4e63954dc799083ee453006
Eliot Howes <ejhowes@gmail.com>
2019-06-02 16:54:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,10 +136,12 @@ def big_shoe_rebounds
             end
           end
         end
-        player_with_biggest_shoes = biggest_shoes.key(biggest_shoes.values.max)
-        if player == player_with_biggest_shoes && stat == :rebounds
-          rebounds << value
-        end
+        attributes.each do |player, stats|
+          player_with_biggest_shoes = biggest_shoes.key(biggest_shoes.values.max)
+            if player == player_with_biggest_shoes && stat == :rebounds
+              rebounds << value
+            end
+          end
       end
     end
   end
