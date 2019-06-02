cf649478b86c1dbaa3d22041d1ee50264f0e1db4
Eliot Howes <ejhowes@gmail.com>
2019-06-02 15:28:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,6 +127,8 @@ def big_shoe_rebounds
   game_hash.each do |home_away, team_data|
     team_data.each do |team, attributes|
       if team == :player
+        attributes.each do ||
+        end
       end
       binding.pry
     end
